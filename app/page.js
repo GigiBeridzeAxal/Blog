ba4369d4b4413a56c51ec18b6c24f6bec7d8aafc
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2024-02-10 19:00:14 +0400

    First Commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Dashoard from './components/Dashoard'
 import Header from './components/Header'
 import About from './components/About'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 import Gigi from './components/Gigi'
 
 
